2d6ab41ed1c96f9739afbd15d5d67718b11905b2
Nic Gaffney <gaffney_nic@protonmail.com>
2025-10-23 21:56:14 -0500

    I honestly have no idea how this is supposed to work

--- build.zig
+++ build.zig
@@ -12,30 +12,32 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
         .target = target,
     });
+    const options = b.addOptions();
+    rootmod.addOptions("build", options);
 
-    const lib = b.addLibrary(.{
-        .name = "funcz",
-        .root_module = rootmod,
-    });
+    // const lib = b.addLibrary(.{
+    //     .name = "funcz",
+    //     .root_module = rootmod,
+    // });
+    //
+    // b.installArtifact(lib);
 
-    b.installArtifact(lib);
+    // const exe = b.addExecutable(.{
+    //     .name = "funczExample",
+    //     .root_module = b.createModule(.{
+    //         .root_source_file = b.path("src/example.zig"),
+    //         .imports = &.{ .{.name = "funcz", .module = lib.root_module} },
+    //         .target = target,
+    //         .optimize = optimize,
+    //     })
+    // });
+    //
+    // b.installArtifact(exe);
 
-    const exe = b.addExecutable(.{
-        .name = "funczExample",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/example.zig"),
-            .imports = &.{ .{.name = "funcz", .module = lib.root_module} },
-            .target = target,
-            .optimize = optimize,
-        })
-    });
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| run_cmd.addArgs(args);
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // const run_cmd = b.addRunArtifact(exe);
+    // run_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| run_cmd.addArgs(args);
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
 
 }
